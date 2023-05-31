d5cf357acebd80aebb9c4b7e4ae177aed845343a
Henrique Baiardi <henriquebaiardi@gmail.com>
2023-05-31 15:31:25 -0300

    [feat]create_Button_for_main_from

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,8 +4,10 @@ const CampoTexto = (props) => {
     
     return (
         <div className="campo-texto">
-            <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <label>
+                {props.label}
+            </label>
+            <input required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 // 
 const Formulario = () => {
@@ -15,15 +16,21 @@ const Formulario = () => {
         'Gestão e Inovação'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Formulario Salvo com Sucesso!')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome..." />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
-                <CampoTexto label="Endereço" placeholder="Digite seu endereço..." />
-                <CampoTexto label="Outros" placeholder="Digite algo que achar nescessario..." />
-                <ListaSuspensa label="Time" itens={times}/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome..." />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo..." />
+                <CampoTexto obrigatorio={true} label="Endereço" placeholder="Digite seu endereço..." />
+                <CampoTexto obrigatorio={false} label="Outros" placeholder="Digite algo que achar nescessario..." />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao > Criar Card </Botao>
             </form>
         </section>
     )
